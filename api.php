810a0a967f33445a1e18ccd854612df6f354695c
Izadora Toledo <izadora.devdescontraida@gmail.com>
2023-01-07 12:35:26 -0300

    refactor: alterando nomeclaturas

--- api.php
+++ api.php
@@ -1,10 +1,14 @@
 <?php 
 
-$url = "https://gateway.marvel.com/v1/public/characters?ts=1000&apikey=108faad5017c38e7e6fbdbab83de752b&hash=1d352958daa20ac6c7f27792c95ce5d2";
+class Api {
+    public static function buscarCep() {
+        $url = "https://gateway.marvel.com/v1/public/characters?ts=1000&apikey=108faad5017c38e7e6fbdbab83de752b&hash=1d352958daa20ac6c7f27792c95ce5d2";
         $curl = curl_init($url);
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         $personagens = curl_exec($curl);   
 
-        $personagens = json_decode($personagens, true);        
-        return $personagens_data; 
+        $Allpersonagens = json_decode($personagens, true);                 
+        return $Allpersonagens; 
+    } 
+}
