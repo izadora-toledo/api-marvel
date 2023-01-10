4d16d0d64cc314813bd984fb885922738fa8bfe1
Izadora Toledo <izadora.devdescontraida@gmail.com>
2023-01-10 15:48:55 -0300

    refactor: terminando de arrumar os cards

--- stories.php
+++ stories.php
@@ -13,17 +13,20 @@ $return_heroi = mysqli_fetch_array($heros);
     <title>Document</title>
 </head>
 <body>
-    <ul class="topnav">
-        <li><a class="active" href="#marvel">MARVEL</a></li>
-        <li class="home"><a class="home" href="index.php">Clique aqui para voltar pro menu de heróis.</a></li>
-    </ul> 
+    <div class="logo">
+        <ul class="topnav">
+            <li><a class="active" href="#marvel">MARVEL</a></li>
+            <li class="home"><a class="home" href="index.php">Clique aqui para voltar pro menu de heróis.</a></li>
+        </ul> 
+    </div>
+   
     <div class="container">
         <div class="col-md-4" id="div_card_stories">
             <div class="card" style="width: 25rem;">           
                 <div class="card-body">
                     <h1><?php echo !empty($return_heroi['nome']) ? 'Histórias do herói ' . $return_heroi['nome'] : 'Você não selecionou nenhum herói!' ?></h1>
                     <?php                         
-                        while($return_storie = mysqli_fetch_array($stories)){?>
+                        while ($return_storie = mysqli_fetch_array($stories)) {?>
                         <h4 for="descricao_historia" id="titulo_historia"><?php echo !empty($return_storie['descricao']) ? $return_storie['titulo_storie'] : '' ?></h4>
                         <p type="text" class="card-text" id="descricao_historia"><?php echo !empty($return_storie['descricao']) ? $return_storie['descricao'] : '' ?></p>                            
                     <?php } ?>                        
